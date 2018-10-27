c93c6e0e1f2d09d96d202e4662d8c9013c24878f
Yuki N <lunarbase@outlook.com>
2018-10-27 04:10:51 +0000

    automatically backed up by learn

--- lib/rescue_princess_peach.rb
+++ lib/rescue_princess_peach.rb
@@ -1,7 +1,7 @@
 def rescue_princess_peach
   status = "rescued"
   puts status
-  puts "Hooray! Mario has rescued Princess Peach."
+  puts "#{status} Hooray! Mario has rescued Princess Peach."
 end
 
 
